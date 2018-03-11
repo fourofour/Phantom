7ee18143ac5a928414a23bef8b27bef837d61f0b
Mehrab Ganji <mehrab.ganji@usa.com>
2018-03-11 17:02:40 +0330

    new compiler, new base
    adding gulp for es6 + node-sass for .sass and also bulma as a base css

---
+++ native/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default', () =>
+  gulp.src('src/app.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist'))
+)
+
+gulp.task('watch', () =>
+  gulp.watch('src/script/**/*.js', ['default'])
+)
