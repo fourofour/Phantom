fbcd8015bfdf323537dbdbdd44f86712cef8295f
Mehrab Ganji <mehrab.ganji@usa.com>
2018-03-18 15:54:02 +0330

    Js Core init
    Init core structure

--- native/gulpfile.js
+++ native/gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () =>
-  gulp.src('src/app.js')
+  gulp.src('src/main.js')
     .pipe(babel({
       presets: ['env']
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/script'))
 )
 
 gulp.task('watch', () =>
-  gulp.watch('src/script/**/*.js', ['default'])
+  gulp.watch('src/main.js', ['default'])
 )
