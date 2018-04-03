ca40e71084768a1565bffb45ea9149e62abc05c8
Mehrab Ganji <mehrab.ganji@usa.com>
2018-04-03 10:03:36 +0430

    Tooltip init
    adding tooltip and some other things such as _live _native _setting ...

--- native/gulpfile.js
+++ native/gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () =>
-  gulp.src('src/main.js')
+  gulp.src('src/phantom.js')
     .pipe(babel({
       presets: ['env']
     }))
-    .pipe(gulp.dest('dist/script'))
+    .pipe(gulp.dest('dist'))
 )
 
 gulp.task('watch', () =>
-  gulp.watch('src/main.js', ['default'])
+  gulp.watch('src/phantom.js', ['default'])
 )
--- native/src/phantom.js
+++ native/src/phantom.js
@@ -2,11 +2,16 @@ class Phantom  {
   constructor () {
     //setting up access to the Phantom Object
     let that = this
+    
+    // Keep our global setting here
+    this.__proto__._setting = {
+      debugMod: true
+    }
 
     // Setting up our core which will be used in all different places such as in JS Native, jQuery and ...
     this.__proto__._core = {
       // function to get elements position
-      getPosition: function ({ el = null }) {
+      getPosition: function (el = null) {
         if (el === null)
           return el
         else
@@ -16,7 +21,7 @@ class Phantom  {
           }
       },
       // function to make elements fast
-      createElement: function ({ type = 'DIV', classList = [], children = [], text = '' }) {
+      createElement: function ({ type = 'DIV', classList = [], children = [], text = '', style = [], attributes = [], attr = attributes }) {
         let element = document.createElement(type)
 
         // add Class list
@@ -33,10 +38,28 @@ class Phantom  {
         if (text.length > 0)
           element.appendChild(document.createTextNode(text))
 
+        // adding inline styles
+        if (style.length > 0) {
+          style.forEach(function (value, index, array) {
+            let key = Object.keys(value)[0]
+
+            element.style[key] = value[key]
+          })
+        }
+
+        //adding custom attributes
+        if (attr.length > 0) {
+          attr.forEach(function (value, index, array) {
+            let key = Object.keys(value)[0]
+
+            element.setAttribute(key, value[key])
+          })
+        }
+
         return element
       },
       init: {
-        add: function ({ name, callback }) {
+        add: function (name, callback) {
           if (name === undefined || callback === undefined || this.list.has(name))
             return null
           else
@@ -51,16 +74,109 @@ class Phantom  {
         clear: function () {
           this.list.clear()
         },
-        list: new Map()
+        list: new Map(),
+        run: function () {
+          this.list.forEach(function (value, index, map) {
+            if (that._setting.debugMod)
+              console.log("Initializing " + index)
+
+            value()
+
+            if (that._setting.debugMod)
+              console.log("Finished " + index)
+          })
+        }
       }
     }
 
-    this.__proto__._native = {
-
+    // We keep out live values here
+    this.__proto__._live = {
+      id: {
+        get: function () {
+          return this.current++
+        },
+        current: 0
+      },
+      tooltip: new Map()
     }
+
+    this.__proto__._native = {
+      /*
+      * Tooltip
+      *
+      *   you can access it by adding following attribute [data-ph-tooltip]
+      * */
+      tooltip: function (querySelector) {
+        let list
+
+        if (querySelector === undefined) {
+          list = document.querySelectorAll('[data-ph-tooltip]')
+        } else {
+          list = document.querySelectorAll(querySelector)
+        }
+
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i]
+
+          item.addEventListener('mouseover', function (event) {
+            let tooltip,
+              position,
+              element,
+              id,
+              height,
+              width,
+              location
+
+            height = item.clientHeight
+            width = item.clientWidth
+
+            if (item.hasAttribute('data-ph-id')) {
+              id = item.getAttribute('data-ph-id')
+            } else {
+              id = that._live.id.get()
+
+              item.setAttribute('data-ph-id', id)
+            }
+
+            position = that._core.getPosition(item)
+
+            location = {
+              top: position.top - height - 5 + 'px',
+              left: position.left + (width / 2) + 'px'
+            }
+
+            tooltip = item.getAttribute('data-ph-tooltip')
+            element = that._core.createElement({
+              classList: ['tooltip'],
+              text: tooltip,
+              style: [{top: location.top}, {left: location.left}]
+            })
+
+            that._live.tooltip.set(id.toString(), element)
+
+            document.body.appendChild(element)
+          }, false)
+
+          item.addEventListener('mouseleave', function(event) {
+            let id = event.target.getAttribute('data-ph-id'),
+              element
+
+            element = that._live.tooltip.get(id)
+
+            element.remove()
+
+            that._live.tooltip.delete(id)
+          }, false)
+        }
+      }
+    }
+
+    this._core.init.add('tooltip', that._native.tooltip)
+
+    this._core.init.run()
   }
 
 }
 
-let phantom = new Phantom('salam')
+let phantom = new Phantom()
 
